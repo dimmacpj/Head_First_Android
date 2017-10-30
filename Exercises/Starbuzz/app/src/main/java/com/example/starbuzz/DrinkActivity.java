67b369234383b406fcf38237bea271952f5de115
neal <neal@ctwest.nz>
2017-10-30 16:43:30 +1300

    chapter 12 page 540

--- Exercises/Starbuzz/app/src/main/java/com/example/starbuzz/DrinkActivity.java
+++ Exercises/Starbuzz/app/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -1,12 +1,16 @@
 package com.example.starbuzz;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.media.Image;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -20,12 +24,26 @@ public class DrinkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink);
         Intent intent = getIntent();
         id = (int)intent.getExtras().get("itemPosition");
-        Drink drink = Drink.drinks[id];
+        //Drink drink = Drink.drinks[id];
         imageView = (ImageView) findViewById(R.id.image_view);
         nameView = (TextView)findViewById(R.id.name_view);
         descriptionView = (TextView)findViewById(R.id.description_view);
-        imageView.setImageResource(drink.getImageResourceId());
+        /*imageView.setImageResource(drink.getImageResourceId());
         nameView.setText(drink.getName());
-        descriptionView.setText(drink.getDescription());
+        descriptionView.setText(drink.getDescription());*/
+        try{
+            StarbuzzDatabaseHelper helper = new StarbuzzDatabaseHelper(this);
+            SQLiteDatabase db = helper.getReadableDatabase();
+            Cursor cursor = db.query("drink", new String[] {"name", "description", "image_resource_id"}, "id = ?", new String[] {Integer.toString(id)}, null, null, null);
+            if (cursor.moveToFirst()){
+                nameView.setText(cursor.getString(cursor.getColumnIndex("name")));
+                descriptionView.setText(cursor.getString(cursor.getColumnIndex("description")));
+                imageView.setImageResource(cursor.getInt(cursor.getColumnIndex("image_resource_id")));
+            }
+            cursor.close();
+            db.close();
+        }catch (SQLiteException e){
+            Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
     }
 }
