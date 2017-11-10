28c9d6ca51033a7a52f133568762be1390e5162a
neal <neal@ctwest.nz>
2017-11-10 16:41:10 +1300

    Chapter 14 page 647

---
+++ Exercises/Pizza/app/src/main/java/com/example/pizza/CaptionedImagesAdapter.java
@@ -0,0 +1,39 @@
+package com.example.pizza;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+/**
+ * Created by neal on 10/11/17.
+ */
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder>{
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
+        public ViewHolder(CardView v){
+            super(v);
+            cardView = v;
+        }
+    }
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_captioned_image, parent, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position, List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
